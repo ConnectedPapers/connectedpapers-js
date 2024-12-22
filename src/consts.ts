e942a2c68cfec1496150f6db04fddca4932ec3d6
aletar89 <aletar89@gmail.com>
2024-12-22 12:37:06 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 const testToken = 'TEST_TOKEN';
-const defaultServerAddress = 'https://api.connectedpapers.com';
+const defaultServerAddress = 'https://rest.prod.connectedpapers.com';
 
 export const accessToken = process.env.CONNECTED_PAPERS_API_KEY ?? testToken;
 export const connectedPapersRestApi = process.env.CONNECTED_PAPERS_REST_API ?? defaultServerAddress;
